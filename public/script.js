dd956ae532d309b487a06d0d77e480c02772d85d
Valentin <valentin.linhardt@gmail.com>
2025-02-06 11:00:33 +0100

    skillset added

--- public/script.js
+++ public/script.js
@@ -10,3 +10,17 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const skillObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)  
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const skillElements = document.querySelectorAll('.skill');
+skillElements.forEach((el) => skillObserver.observe(el));
